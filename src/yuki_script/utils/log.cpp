ebe92459e8df919c81d2e7195cdad4e8de770d4c
Leo <leo.ghafari@gmail.com>
2018-03-07 15:28:19 +0900

    Added a small basis for a log utility

---
+++ src/yuki_script/utils/log.cpp
@@ -0,0 +1,10 @@
+#include "log.hpp"
+
+#include <iostream>
+
+
+void yuki::utils::log(const std::string& out)
+{
+	std::cout << out << std::endl;
+}
+
---
+++ src/yuki_script/utils/log.hpp
@@ -0,0 +1,21 @@
+#ifndef YUKI_SCRIPT_UTILS_LOG_HPP
+#define YUKI_SCRIPT_UTILS_LOG_HPP
+
+#include <string>
+
+
+namespace yuki::utils
+{
+
+	void log(const std::string & out);
+
+}
+
+#endif
+
+
+
+
+
+
+
